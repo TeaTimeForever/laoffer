39ea0d01de5a39dc3ac982ffcf0ef2512add03e8
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2016-10-11 23:12:12 +0300

    form component for new point creation

---
+++ client/imports/app/profile/points-management/point-form/point-form.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { Router } from "@angular/router";
+import { Point } from "../../../../../../both/models/point";
+import { Address } from "../../../../../../both/models/address";
+import { PointCollection } from "../../../../../../both/collections/point.collection";
+
+@Component({
+  selector: 'point-form',
+  template: `
+    <h3>Please add info about your new point</h3>
+    <form (submit)="addNewPoint()">
+        <address-fieldset [address]="point.address"></address-fieldset>
+        <input [(ngModel)]="point.phone"
+               type="text"
+               name="phone"
+               placeholder="phone" /><br>
+         
+    </form>
+    <button (click)="addNewPoint()" >add new point</button>
+`
+})
+export class PointFormComponent implements OnInit {
+  @Input()
+  private point: Point;
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+    if (!this.point) {
+      console.log("POINT", this.point);
+      this.point = new Point();
+      this.point.address = new Address();
+    }
+    console.log("LOADED POINT", this.point);
+  }
+
+  addNewPoint(): void {
+    console.log("submit processing");
+    if(!Meteor.userId()) {
+      alert("please login");
+    } else {
+      console.log("POINT ON SUBMIT", this.point);
+      PointCollection.insert(this.point);
+    }
+
+
+  }
+}
