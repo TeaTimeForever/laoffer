ae2813b54882a5172937bc8fc9f4503bbf60726c
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2016-10-11 23:11:44 +0300

    point interface convert to class

--- both/models/point.ts
+++ both/models/point.ts
@@ -1,10 +1,14 @@
 import ObjectID = Mongo.ObjectID;
 import { Address } from "./address";
 
-export interface Point {
-  _id?: ObjectID,
-  companyId: ObjectID,
+export class Point {
+  _id?: ObjectID;
+  companyId: ObjectID;
   geoLocation: string;
-  address?: Address; // TODO: implement Address class
+  address?: Address;
   phone: string;
+
+  constructor(){
+    this.companyId = (<any>Meteor.user()).companyId;
+  }
 }
