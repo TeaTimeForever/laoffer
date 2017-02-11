8299e80b61e3757fda8d4b4a0866db5f443b4e3e
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2017-02-11 20:26:32 +0200

    routes for concrete points

--- client/imports/app/profile/user-profile.routes.ts
+++ client/imports/app/profile/user-profile.routes.ts
@@ -5,7 +5,9 @@ import { OfferListComponent } from "./offer-views/offer-list.component";
 const children = {
   toModuleHome: {path: "", component: PointListComponent },
   toOffers:  {path: "offers", component: OfferListComponent },
-  toConcreteOffer:  {path: "offers/:id", component: OfferListComponent }
+  toConcreteOffer:  {path: "offers/:id", component: OfferListComponent },
+  toPoints:  {path: "points", component: PointListComponent },
+  toConcretePoint:  {path: "points/:id", component: PointListComponent }
 };
 
 export var profileRoutes: any = Object.create(children);
